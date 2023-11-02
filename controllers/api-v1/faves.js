6caa1384b254d48282ceea539bd8d3d3339ad00f
jtw007 <joshwu095@gmail.com>
2023-11-02 15:40:16 -0700

    created faves.js controller

---
+++ controllers/api-v1/faves.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const db = require('../../models')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const authLockedRoute = require('./authLockedRoute')
+
+
+
+module.exports = router
